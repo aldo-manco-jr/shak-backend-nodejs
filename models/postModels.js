eba850a1cdc399afd67e5ecef64ae67e769f5dd1
aldo-manco-jr <aldo.manco@protonmail.ch>
2020-07-14 13:12:19 +0200

    'post_schema'

---
+++ models/postModels.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    username: { type: String, default: ''},
+    post_content: { type: String, default: ''},
+    comments:[
+        {
+            user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+            username: { type: String, default: ''},
+            comment_text: { type: String, default: ''},
+            created_at: { type: Date, default: Date.now()}
+        }
+    ],
+    total_likes: { type: Number, default: 0 },
+    likes: [
+        {
+            username: { type: String, default:''}
+        }
+    ],
+    created_at: { type: Date, default: Date.now() }
+});
+
+module.exports = mongoose.model('Post', postSchema);
