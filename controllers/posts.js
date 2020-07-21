ac34b6f118ef99ff974e90fb34b273f347ef58f0
aldo-manco-jr <aldo.manco@protonmail.ch>
2020-07-21 13:43:34 +0200

    'likes'

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,59 +6,85 @@ const users = require('../models/userModels');
 
 module.exports = {
 
-    AddPost(req, res) {
-        const schemaPost = Joi.object().keys({
-            post: Joi.string().required()
-        });
+  AddPost(req, res) {
+    const schemaPost = Joi.object().keys({
+      post: Joi.string().required()
+    });
 
-        const {error} = Joi.validate(req.body, schemaPost);
+    const { error } = Joi.validate(req.body, schemaPost);
 
-        if (error && error.details) {
-            return res
-                .status(HttpStatus.BAD_REQUEST)
-                .json({msg: error.details});
-        }
-
-        const body = {
-            user_id: req.user._id,
-            username: req.user.username,
-            post: req.body.post,
-            created_at: new Date()
-        }
-
-        posts.create(body)
-            .then(async post => {
-                await users.updateOne({
-                    _id: req.user._id
-                }, {
-                    $push: {
-                        posts: {
-                            postId: post._id,
-                            post: req.body.post,
-                            created_at: new Date()
-                        }
-                    }
-                })
-                res.status(HttpStatus.OK).json({message: 'Post created successfully', post});
-            }).catch(err => {
-            res
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .json({message: 'Error occured'});
-
-        });
-    },
-
-    async GetAllPosts(req, res) {
-
-        try {
-            const allPosts = await posts.find({})
-                .populate('user_id')
-                .sort({ created_at: -1 });
-
-            return res.status(HttpStatus.OK).json({message: 'All posts', allPosts});
-        } catch (err) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'});
-        }
+    if (error && error.details) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ msg: error.details });
     }
+
+    const body = {
+      user_id: req.user._id,
+      username: req.user.username,
+      post: req.body.post,
+      created_at: new Date()
+    };
+
+    posts.create(body)
+      .then(async post => {
+        await users.updateOne({
+          _id: req.user._id
+        }, {
+          $push: {
+            posts: {
+              postId: post._id,
+              post: req.body.post,
+              created_at: new Date()
+            }
+          }
+        });
+        res.status(HttpStatus.OK).json({ message: 'Post created successfully', post });
+      }).catch(err => {
+      res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: 'Error occured' });
+
+    });
+  },
+
+  async GetAllPosts(req, res) {
+
+    try {
+      const allPosts = await posts.find({})
+        .populate('user_id')
+        .sort({ created_at: -1 });
+
+      return res.status(HttpStatus.OK).json({ message: 'All posts', allPosts });
+    } catch (err) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error occured' });
+    }
+  },
+
+  async AddLike(req, res) {
+
+    const postId = req.body._id;
+
+    await posts.updateOne({
+        _id: postId,
+        'likes.username': { $ne: req.user.username}
+      },
+      {
+        $push: {
+          likes: {
+            username: req.user.username
+          }
+        },
+        $inc: {
+          total_likes: 1
+        }
+      })
+      .then(() => {
+        return res.status(HttpStatus.OK).json({message: 'You have liked the post'});
+      })
+      .catch((err) => {
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'});
+      });
+  }
 
 };
